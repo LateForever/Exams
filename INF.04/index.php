a43f9c52574899247bc89305994c7e8050e39f03
LateForever <albertgrzegrzolka124@gmail.com>
2023-06-02 10:48:36 +0200

    almost ended php

--- INF.04/index.php
+++ INF.04/index.php
@@ -2,7 +2,7 @@
 <html lang="pl">
     <head>
         <meta charset="UTF-8"/>
-        <link href="./style.css" rel="stylesheet" />
+        <link href="./style.css" rel="stylesheet"/>
         <title>Obiekty sportowe</title>
     </head>
     <body>
@@ -21,7 +21,7 @@
                 </ul>
             </nav>
         </haeder>
-        <main>
+        <div class="main">
             <div class="main-left">
                 <ul>
                 <?php
@@ -41,7 +41,7 @@
                             $id = $res["id"];
                             $name = $res["nazwa"];
 
-                            echo "<li><a href='/index.php?=$id' target='_self'>$name</a></li>";
+                            echo "<li><a href='./index.php?=$id' target='_self'>$name</a></li>";
                         }
                     }
 
@@ -50,11 +50,86 @@
                 </ul>
             </div>
             <div class="main-middle">
+                <?php
+
+                    if(isset($_GET["id"])) {
+
+                        $id = $_GET["id"];
+
+                        $con = new mysqli("localhost", "root", "", "sport");
+
+                        if($con->connect_error) {
+                            die("Database connect error" . $con->connect_error);
+                        }
+
+                        $sql = "SELECT os.nazwa, os.adres, os.godzinyOtwarcia, os.obrazek, ro.nazwa AS obiekt FROM ObiektSportowy AS os
+                        INNER JOIN RodzajObiektu AS ro ON ro.id = os.rodzajObiektu
+                        WHERE os.id =$id";
+
+                        $result = $con->query($sql);
+
+                        if($result->num_rows > 0) {
+
+
+
+                        } else {
+                            
+                            $sql1 = "SELECT os.nazwa, os.adres, os.godzinyOtwarcia, os.obrazek, ro.nazwa AS obiekt FROM ObiektSportowy AS os
+                            INNER JOIN RodzajObiektu AS ro ON ro.id = os.rodzajObiektu";
+
+                            $result1 = $con->query($sql1); 
+
+                            while($res1 = $result1->fetch_assoc()) {
+
+                                $name = $res1["nazwa"];
+                                $adres = $res1["adres"];
+                                $godzinyOtwarcia = $res1["godzinyOtwarcia"];
+                                $obrazek = $res1["obrazek"];
+                                $obiekt = $res1["obiekt"];
+
+                                echo "<h2>$name</h2><h3>$obiekt</h3><p>$adres</p><img src='/img/$obrazek'/><p>$godzinyOtwarcia</p>";
+                            }
+                        }
+                    }
+
+                ?>
             </div>
             <div class="main-right">
                 <h2>Dodaj nowy obiekt</h2>
                 <form method="POST">
-                    
+                    <label for="nazwa">
+                        nazwa obiektu
+                    </label>
+                    <input type="text" name="nazwa" id="nazwa" />
+                    <label for="adres">
+                        adres obiektu
+                    </label>
+                    <input type="text" name="adres" id="adres" />
+                    <label for="rodzaj">
+                        rodzaj obiektu
+                    </label>
+                    <select name="rodzaj" id="rodzaj">
+                        <?php
+                            $con = new mysqli("localhost", "root", "", "Sport");
+
+                            if($con->connect_error) {
+                                die("Mysql connection error" . $con->connect_error);
+                            }
+
+                            $sql = "SELECT id, nazwa FROM RodzajObiektu;";
+
+                            $res = $con->query($sql);
+                            if($res->num_rows > 0) {
+                                while($row = $res->fetch_assoc()) {
+
+                                    $id = $row["id"];
+                                    $name = $row["nazwa"];
+
+                                    echo "<option value='$name'>$name</option>";
+                                }
+                            }
+                        ?>
+                    </select>
                 </form>
                 <?php
 
@@ -75,19 +150,17 @@
                         $sql = "INSERT INTO ObiektSportowy(nazwa, adres, rodzajObiektu, godzinyOtwarcia, obrazek)
                         VALUES ('$object_name', '$object_address', $object_type, '$object_hours', '$_object_image');";
 
-                        if($con->query($sql) === TRUE) {
-                            
-                        }  else {
+                        if($con->query($sql) === FALSE) {
                             echo "Insert query error" . $con->error;
                         }
 
-                        $con->close();
+                        $con->close(); 
                     
                     }
 
                 ?>
             </div>
-        </main>
+        </div>
         <footer>
             <p>&copy 67899076889</p>
         </footer>
